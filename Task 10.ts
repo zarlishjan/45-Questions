148f29388280940672781985aadc7d00f3e3a1b3
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:36:10 +0500

    Add files via upload

---
+++ Task 10.ts
@@ -0,0 +1,8 @@
+// Shahid Hussain Jokhio
+// Tuesday 14th May 2024.
+
+// here is my favourite number
+const favouriteNumber = 5;
+// Here is my message. 
+const Message = `This is my fvourite number ${favouriteNumber} `
+console.log (Message)
