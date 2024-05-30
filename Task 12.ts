d654f9e76f4a2058cfab0e9d3524189e2841638c
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:37:18 +0500

    Add files via upload

---
+++ Task 12.ts
@@ -0,0 +1,5 @@
+let studentNames: string [] = [`Shahid`, `Zahid`, `sarmad`, `hassan`]
+console.log (`here is the name of student ${studentNames [0]}`);
+console.log (`here is the name of student ${studentNames [1]}`);
+console.log (`here is the name of student ${studentNames [2]}`);
+console.log (`here is the name of student ${studentNames [3]}`);
