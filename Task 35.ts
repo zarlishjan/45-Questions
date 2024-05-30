9bb254d98fec7446e386cf85b31a63082c58800e
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:53:59 +0500

    Add files via upload

---
+++ Task 35.ts
@@ -0,0 +1,5 @@
+let myAnimals = [`Dog`, `Cat`, `Goat`];
+myAnimals.forEach(myAnimals => {
+    console.log (`A ${myAnimals} would make a great pet`)
+});
+console.log (`Any of these Animals would make a great pet`)
