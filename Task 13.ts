bcf72383629d8bfe0daede09b58702ece7f9f631
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:38:05 +0500

    Add files via upload

---
+++ Task 13.ts
@@ -0,0 +1,5 @@
+let nameofVehicles: string [] = [`Heavy Bike`, `Honda Civic`, `Honda 125`, `Ferrari`]
+console.log (`I Would like my own a  ${nameofVehicles [0]}`);
+console.log (`I Would like my own a ${nameofVehicles [1]}`);
+console.log (`I Would like my own a ${nameofVehicles [2]}`);
+console.log (`I Would like my own a ${nameofVehicles [3]}`);
