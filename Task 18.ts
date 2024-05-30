bc105f94403efa58d127518bdea602646aa06f37
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:40:57 +0500

    Add files via upload

---
+++ Task 18.ts
@@ -0,0 +1,24 @@
+
+
+let favouritecities : string [] = [`Faisal Mosque`, `Margalla Hills`, `Gorakh Hill`, `Maree`, `Kashmir` ]
+console.log (favouritecities)
+console.log (`\n ALPHABETICAL ORDER  `)
+console.log ( [...favouritecities].sort())
+
+console.log ("original status : " + favouritecities);
+
+console.log ("Alphabets are in reverse order :"+[...favouritecities].reverse())
+
+console.log ("\n original status : " + favouritecities);
+
+console.log ("\n Reverse the order of list   \t" + favouritecities.reverse())
+
+console.log ("\n Reverse the order of list   \t" + favouritecities.reverse())
+
+console.log ("Alphabetical Orders " + favouritecities.sort())
+
+
+console.log ("Alphabetical Reverse Orders " + favouritecities.sort().reverse())
+// • Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+
+// • Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
