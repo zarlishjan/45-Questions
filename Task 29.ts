22689c2f5e25ab996129fcbcda94ef984399d36d
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:49:54 +0500

    Add files via upload

---
+++ Task 29.ts
@@ -0,0 +1,25 @@
+let favouritefruits = [`Apple`, `Mango`, `Banana`]
+if (favouritefruits.includes (`Apple`)){
+
+    console.log (`\n I Really Like Apples`)
+}
+
+if (favouritefruits.includes (`Mango`)){
+
+    console.log (`\n I Really Like Mango`)
+}
+
+if (favouritefruits.includes (`Banana`)){
+
+    console.log (`\n I Really Like Banana`)
+}
+
+if (favouritefruits.includes (`Strawberry`)){
+
+    console.log (`\n I don't Like Strawberry`)
+}
+
+if (favouritefruits.includes (`Guava`)){
+
+    console.log (`\n I Don't Like Guava`)
+}
