fbbf490a2f35d8c05e289e61c354d5a154ba847b
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:55:14 +0500

    Add files via upload

---
+++ Task 38.ts
@@ -0,0 +1,6 @@
+function describe_City (city: string, country: string = "Pakistan"){
+    console.log (`${city} is in ${country}`)
+}
+describe_City(`Karachi`);
+describe_City(`France`, `Europe`);
+describe_City (`Jedha`, `Saudi Arabia`);
