61231c3ee8e8fd6646261be94f45a982bbd2f4b0
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:54:25 +0500

    Add files via upload

---
+++ Task 36.ts
@@ -0,0 +1,4 @@
+function make_shirt(size: string, message :string){
+    console.log (`Making a Size ${size} T-Shirt with message ${message} Printed on it`)
+}
+make_shirt ("Medium", "Code is Life")
