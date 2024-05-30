20d7c52f1f428650dba2938d6575521ec0b43a06
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:42:39 +0500

    Add files via upload

---
+++ Task 20.ts
@@ -0,0 +1,9 @@
+// Think of something you could store in a array. For example, 
+// you could make a list of mountains, rivers, countries, cities, languages, or 
+// anything else you’d like. Write a program that creates a list containing these items.
+
+let places : string [] = [`K2`, `Indus River`, `Thatta`, `Urdu`]
+for (let i=0; i<places.length; i++)
+    {
+        console.log (places[i])
+    }
