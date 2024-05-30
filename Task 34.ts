58cbc7eb6d4b5d928bf0a2c102cdaa3602550d27
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:53:27 +0500

    Add files via upload

---
+++ Task 34.ts
@@ -0,0 +1,10 @@
+let myPizza = [`Vegetable Pizza`, `Malai Boti Pizza`, `Spicy Pizza`];
+for (let i = 0; i<myPizza.length; i++){
+    console.log (myPizza[i])
+}
+
+console.log ("\n\t With Sentence \n\t")
+
+for (let i=0; i<myPizza.length; i++) {
+    console.log (`i Would like to eat ${myPizza[i]}`)
+}
