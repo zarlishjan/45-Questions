9dd50710c08992f20df474c82c3b3d43c4e64d03
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:45:04 +0500

    Add files via upload

---
+++ Task 23.ts
@@ -0,0 +1,36 @@
+let car = 'subaru';
+
+console.log("\n Is car == 'subaru'? I predict True.")
+console.log (`\n Five Conditions True`)
+
+console.log (`\n Is Car == Subaru"`)
+console.log(car == 'subaru')
+
+console.log (`\n Is Car not equals to "Van"`)
+console.log (car != "Van")
+
+console.log (`\n Is Car Greater than 4`)
+console.log (car.length > 4)
+
+console.log (`\n Is Car Greater then or equals to 3`)
+console.log (car.length >= 3)
+
+console.log (`\n Is Car less than or equals to 3"`)
+console.log (car.length <= 7)
+
+console.log (`\n Five Conditions False`)
+
+console.log (`\n Is Car equals to van"`)
+console.log(car == 'van');
+
+console.log (`\n Is Car less than 1 "`)
+console.log (car.length <1);
+
+console.log (`\n Is Car starts with n"`)
+console.log (car.startsWith (`n`));
+
+console.log (`\n Is Car greater than or euqals to 7`)
+console.log (car.length >= 7)
+
+console.log (`\n Is Car not equals to subaru`)
+console.log (car != "subaru")
