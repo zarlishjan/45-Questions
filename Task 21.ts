af09d1a02ef26cef261ff0a9a2debb1ded3fc74f
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:43:33 +0500

    Add files via upload

---
+++ Task 21.ts
@@ -0,0 +1,9 @@
+interface type {
+    Name: string; Age: Number;  Profession: string 
+
+}
+let OBJ: type = {Name: `Shahid`, Age: 34, Profession: `Developer`};
+console.log (`\n Object containing my Information : \n`)
+console.log (`my name ${OBJ.Name}`);
+console.log ((`my Age ${OBJ.Age}`));
+console.log (`My Profession ${OBJ.Profession}`);
