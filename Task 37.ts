e5f2f2c98b81b74978cb4ebb9da7b676177de42b
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:54:49 +0500

    Add files via upload

---
+++ Task 37.ts
@@ -0,0 +1,7 @@
+function make_Shirt (size: string = "Large", message: string = "I love type script"){
+    console.log (`Making a ${size} T-Shirt with message ${message} printed on it`)
+
+}
+make_Shirt ();
+make_Shirt ("Medium");
+make_Shirt ("Small", "Dive into Coding");
