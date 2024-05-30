93e8dde9b715660dab40da72979eab4cef0bfb5f
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:47:36 +0500

    Add files via upload

---
+++ Task 26.ts
@@ -0,0 +1,17 @@
+console.log (`\n 1st Part`)
+let alien_color = "Red"
+if (alien_color == `Red`){
+    console.log (`Congratulations you got 5 Points`)
+}
+else {
+    console.log (`Congratulations you got 10 Points`)
+}
+
+console.log (`\n 2nd Part`)
+let alien_colors = "Red"
+if (alien_colors == `Green`){
+    console.log (`Congratulations you got 5 Points`)
+}
+else {
+    console.log (`Congratulations you got 10 Points`)
+}
