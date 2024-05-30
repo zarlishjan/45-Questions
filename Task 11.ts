5b388123877d7283dedda566e65450896e785b17
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:36:47 +0500

    Add files via upload

---
+++ Task 11.ts
@@ -0,0 +1,5 @@
+let studentNames: string [] = [`Shahid`, `Zahid`, `sarmad`, `hassan`]
+console.log (studentNames [0]);
+console.log (studentNames [1]);
+console.log (studentNames [2]);
+console.log (studentNames [3]);
