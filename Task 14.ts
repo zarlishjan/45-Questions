b5d8be2fc78afb5413c178d14453636bff2fc025
zarlishjan <165889609+zarlishjan@users.noreply.github.com>
2024-05-30 09:38:41 +0500

    Add files via upload

---
+++ Task 14.ts
@@ -0,0 +1,5 @@
+let guestlist : string [] = [`Akhter`, `irfan`, `suleman`, `azhar`]
+for (let i=0; i<guestlist.length; i++ )
+    {
+        console.log(`Dear ${guestlist[i]} \n\t You are invited today dinner`)
+    }
